3b6922971475ddc068644dce1d2e646427627d9d
Jeon Seung Hwan <fabronjeon@naver.com>
2026-03-06 09:19:45 +0900

    Add OMX project guidance and update key generation script

--- generate_keys.js
+++ generate_keys.js
@@ -1,5 +1,8 @@
-const webpush = require('web-push');
-const fs = require('fs');
-const vapidKeys = webpush.generateVAPIDKeys();
-fs.writeFileSync('keys.json', JSON.stringify(vapidKeys, null, 2), 'utf8');
-console.log('Keys generated');
+(async () => {
+  const fs = await import('node:fs');
+  const { default: webpush } = await import('web-push');
+  const vapidKeys = webpush.generateVAPIDKeys();
+  fs.writeFileSync('keys.json', JSON.stringify(vapidKeys, null, 2), 'utf8');
+  console.log('Keys generated');
+})();
+  
